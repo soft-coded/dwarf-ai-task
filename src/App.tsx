92010e149a642b9cf95bc06727f248d9cc6570e0
soft-coded <shrutanten.work@gmail.com>
2023-05-11 03:38:11 +0530

    Install tailwind and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import Homepage from "./pages/home/Homepage";
+
 export default function App() {
-	return <div>Hello World</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+		</Routes>
+	);
 }
---
+++ src/pages/home/Homepage.tsx
@@ -0,0 +1,3 @@
+export default function Homepage() {
+	return <div>Hello World</div>;
+}
